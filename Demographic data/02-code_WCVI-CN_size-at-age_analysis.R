03cc03037089f1a7fdd973ebea9442d39a46fd67
Brown <Nicholas.Brown@dfo-mpo.gc.ca>
2024-03-22 15:20:03 -0700

    Got through data collation/cleaning and into plotting

--- Demographic data/02-code_WCVI-CN_size-at-age_analysis.R
+++ Demographic data/02-code_WCVI-CN_size-at-age_analysis.R
@@ -18,6 +18,35 @@ library(here)
 # Save list of all possible NA strings
 na_strings <- c("", " ", "N/A", "#N/A", "NA", "N/S", "na")
 
+# List of WCVI river (and hatchery) names
+wcvi_cn_rivers <- tibble(
+  wcvi_names = c(
+    "artlish", "ash", "bedwell", "burman","cayeghle",
+    "clemens", "colonial", "colonial cayeghle","conuma", "cypre", "effingham", "gold",
+    "great central lake", "kaouk", "kauwinch", "kennedy", "leiner",
+    "lowry", "marble", "megin", "moyeha", "nahmint", "nitinat",
+    "omega pacific", "robertson", "san juan", "sarita", "sproat",
+    "stamp", "tahsis", "tahsish", "thornton", "tofino", "toquaht",
+    "tranquil", "ursus", "zeballos"
+  )
+)
+
+
+# Function to clean stock IDs
+clean_stock_id <- function(stock_name) {
+  
+  clean_name <- stock_name |> 
+    #remove anything preceding a dash, and "(assumed)"
+    str_remove_all("^.*(?=-)|\\(assumed\\)") |> 
+    str_replace_all("[:punct:]", " ") |> # Remove any punctuation characters
+    # Remove all versions of river, hatchery, etc from names
+    str_remove_all("(?i)\\b(river|hatchery|creek|fishway|dam|system|inlet|harbour|seapen|r|cr|h)\\b") |> 
+    str_replace_all("[:space:]+", " ") |> # Collapse all spaces to single
+    str_trim() |> # Remove leading/trailing blank spaces
+    str_to_lower()
+  
+  return(clean_name)
+}
 
 
 # Load historical ENPRO data from 1992-2018 -------------------------------
@@ -80,14 +109,20 @@ enpro_data <- list.files(
       is.na(enpro_source_facility) & sample_source %in% c("River Assessment", "Hatchery Assessment"), 
       site_river_location, 
       enpro_source_facility
-    ),
-    across(everything(), parse_guess)
+    ) |> 
+      clean_stock_id(),
+    across(everything(), parse_guess),
+    origin = if_else(is.na(adipose_fin_clip) & is.na(enpro_source_facility), "unknown", "hatchery")
   ) |> 
   rename(
     "poh_length" = post_orbital_hypural_poh,
-    "nose_fork_length" = fork_length_nose_fork
+    "nose_fork_length" = fork_length_nose_fork,
+    "sex" = sex_final,
+    "area" = statarea,
+    "sample_date" = sample_start_date
   )
 
+  
 
 # Investigate potential conversions between other lengths to poh
 poh_conv_models <- enpro_data |> 
@@ -135,18 +170,17 @@ convert_to_poh <- function(measurement, input = c("fl", "sl")) {
     stop("Input must be 'fl' or 'sl'") # Throw error if input is incorrectly specified
   } 
   
-  pred_data <- mutate(
-    .data = poh_conv_models,
-    val = measurement,
-    pred = `(Intercept)` + val*length2
-  )
+  fl_intercept <- poh_conv_models[poh_conv_models$length_type == "nose_fork_length",]$`(Intercept)`
+  fl_x <- poh_conv_models[poh_conv_models$length_type == "nose_fork_length",]$length2
+  sl_intercept <- poh_conv_models[poh_conv_models$length_type == "standard_length",]$`(Intercept)`
+  sl_x <- poh_conv_models[poh_conv_models$length_type == "standard_length",]$length2
   
-
-    if(input == "fl") {
+  
+  if(input == "fl") {
     # These values from a linear regression of NF on POH; R squared of 0.97
-    return(pred_data[pred_data$length_type == "nose_fork_length",]$pred)
+    return(fl_intercept + measurement*fl_x)
   } else {
-    return(pred_data[pred_data$length_type == "standard_length",]$pred)
+    return(sl_intercept + measurement*sl_x)
   }
   # On average, the conversion will err by ~3%
   
@@ -189,7 +223,8 @@ crest_data <- crest_files |>
       is.na(resolved_stock_origin) & !is.na(oto_stock) ~ oto_stock,
       is.na(resolved_stock_origin) & is.na(oto_stock) & !is.na(dna_results_stock_1) ~ dna_results_stock_1,
       TRUE ~ resolved_stock_origin
-    ),
+    ) |> 
+      clean_stock_id(),
     hatchery_origin = case_when(
       is.na(hatchery_origin) & resolved_stock_source == "CWT" ~ "Y",
       is.na(hatchery_origin) & resolved_stock_source == "CWT" ~ "Y",
@@ -198,7 +233,7 @@ crest_data <- crest_files |>
     # All creel data are in FL
     poh_length_mm = if_else(
       sample_type == "Sport",
-      convert_fl_poh(length_mm, input = "fl"),
+      convert_to_poh(length_mm, input = "fl"),
       length_mm
     )
   ) |> 
@@ -224,6 +259,14 @@ crest_data <- crest_files |>
     cwt_brood_year,
     dna_results_stock_1,
     prob_1
+  ) |> 
+  rename(
+    "resolved_stock_id" = resolved_stock_origin,
+    "sample_date" = collection_date,
+    "area" = area_name,
+    "sample_source" = sample_type,
+    "poh_length" = length_mm,
+    "origin" = hatchery_origin
   )
 # This file includes some escapement, broodstock, and FSC data - to my surprise. 
 # Keep it or drop it?
@@ -235,7 +278,7 @@ crest_data <- crest_files |>
 
 
 # Escapement data from StAD excel records, collated and cleaned in R by Katie Davidson
-esc_data <- list.files(
+stad_data <- list.files(
   here("Demographic data"),
   pattern = "(?i)escapement-fsc",
   full.names = TRUE
@@ -284,7 +327,8 @@ esc_data <- list.files(
     ) |> 
   mutate(
     sample_date = sample_start_date_dd_mm_yyyy,
-    resolved_age = resolved_total_age
+    resolved_age = resolved_total_age,
+    resolved_stock_id = clean_stock_id(resolved_stock_id)
   )
 
 
@@ -319,7 +363,7 @@ rch_f_data <- list.files(
   ) |> 
   mutate(
     oto_nums_concat = paste(lab_number, box_code, sep = "-"),
-    resolved_stock_id = "Robertson Cr",
+    resolved_stock_id = clean_stock_id("Robertson Cr"),
     sample_date = sample_start_date
   ) |> 
   rename(
@@ -355,7 +399,7 @@ merge_data |>
 
 
 # Collate subsetted columns from the two dataframes 
-merged_esc_fsc <- merge_data |> 
+merge1 <- merge_data |> 
   map(
     ~ .x |> 
       mutate(across(everything(), as.character)) |> 
@@ -375,66 +419,99 @@ merged_esc_fsc <- merge_data |>
 
 
 
-# Collate CREST data with escapement/FSC data -----------------------------
+# Collate ENPRO data with escapement/rch fem data -------------------------
 
-# Check whether any overlap between samples in merged_data and crest data
 
+# Check the degree of overlap in years
+merge2_rule <- enpro_data |> 
+  mutate(year = as.character(year)) |> 
+  count(resolved_stock_id, year, name = "enpro_n") |> 
+  full_join(count(merge1, year, resolved_stock_id)) |> 
+  # Stipulate rules for keeping one dataset versus the other |> 
+  mutate(
+    to_keep = case_when(
+      is.na(n) & !is.na(enpro_n) ~ "enpro",
+      !is.na(n) & is.na(enpro_n) ~ "other",
+      enpro_n > n ~ "enpro",
+      n >= enpro_n ~ "other"
+    )
+  ) %>% 
+  split(.$to_keep)
+
+
+# Merge ENPRO data with the previous section's data
+merge2 <- enpro_data |> 
+  mutate(
+    dataset = "enpro",
+    across(everything(), as.character)
+  ) |> 
+  select(colnames(merge1)) |> 
+  # Remove the rows that will come from the other data
+  anti_join(select(merge2_rule$other, year, resolved_stock_id)) |> 
+  bind_rows(
+    merge1 |> 
+      anti_join(select(merge2_rule$enpro, year, resolved_stock_id))
+  )
+
+
+# Collate CREST data with escapement/FSC/ENPRO data -----------------------------
+
+
+# Look at the types of esc/fsc samples in CREST
+crest_data |> 
+  mutate(year = as.character(year)) |>
+  filter(str_detect(sample_source, "(?i)esc|brood|fsc")) |> 
+  count(resolved_stock_id, area) |> 
+  arrange(desc(n)) |> 
+  print(n=200)
+# The bulk of the samples don't have any stock or area information associated
+
+
+# Check degree of overlap between samples in merged_data and crest data
+# and stipulate rule for keeping data
+merge3_rule <- crest_data |> 
+  mutate(year = as.character(year)) |>
+  filter(str_detect(sample_source, "(?i)esc|brood|fsc")) |> # Only look at the escapement/fsc samples
+  count(resolved_stock_id, year, sample_source, name = "crest_n") |> 
+  full_join(count(merge2, year, resolved_stock_id)) |> 
+  # Trim data so only the rows to remove from CREST remain
+  filter(!is.na(crest_n) & !is.na(n)) %T>%
+  {print(arrange(., desc(crest_n)), n = 50)} |> 
+  # In some instances, there is a lot of CREST data and very little in
+  # the other dataset, so we'll keep those CREST data
+  filter(
+    # These data are likely in *EPRO*
+    !(resolved_stock_id == "nitinat" & year %in% c("2020", "2019") & sample_source == "Escapement"),
+    !(resolved_stock_id == "conuma" & year == "2018" & sample_source == "Escapement"),
+    !(resolved_stock_id == "gold" & year %in% c("2015", "2017") & sample_source == "Escapement"),
+    !(resolved_stock_id == "tahsis" & year =="2018" & sample_source == "Escapement"),
+    !(resolved_stock_id == "leiner" & year =="2017" & sample_source == "Escapement")
+  ) %T>%
+  print()
 
 
 # Adjust column names in crest dataset and merge with the esc/fsc data
 full_data <- crest_data |> 
-  rename(
-    "resolved_stock_id" = resolved_stock_origin,
-    "sample_date" = collection_date,
-    "area" = area_name,
-    "sample_source" = sample_type,
-    "poh_length" = length_mm,
-    "origin" = hatchery_origin
-  ) |> 
   mutate(
     dataset = "CREST/FOS",
     across(everything(), as.character)
   ) |> 
-  select(colnames(merged_esc_fsc)) |> 
-  bind_rows(merged_esc_fsc)
+  anti_join(merge3_rule) |> 
+  select(colnames(merge2)) |> 
+  bind_rows(merge2)
 
 
 
 # Trim non-WCVI origin fish from the full data ----------------------------
 
-# List of WCVI river (and hatchery) names
-wcvi_cn_rivers <- tibble(
-  wcvi_names = c(
-    "artlish", "ash", "bedwell", "burman","cayeghle",
-    "clemens", "colonial", "colonial cayeghle","conuma", "cypre", "effingham", "gold",
-    "great central lake", "kaouk", "kauwinch", "kennedy", "leiner",
-    "lowry", "marble", "megin", "moyeha", "nahmint", "nitinat",
-    "omega pacific", "robertson", "san juan", "sarita", "sproat",
-    "stamp", "tahsis", "tahsish", "thornton", "tofino", "toquaht",
-    "tranquil", "ursus", "zeballos"
-  )
-)
-
 
 # Create lookup table to bridge messy stock names to clean WCVI names
 stock_cleanup <- full_data |> 
   count(dataset, resolved_stock_id) |> 
-  # First need to standardize stock of origin names
-  mutate(
-    cleaned_stock_id = str_remove_all(
-      resolved_stock_id,
-      "^.*(?=-)|\\(assumed\\)" # remove anyhing preceding a dash, and "(assumed)"
-    ) |> 
-      str_replace_all("[:punct:]", " ") |> # Remove any punctuation characters
-      # Remove all versions of river, hatchery, etc from names
-      str_remove_all("(?i)\\b(river|hatchery|creek|fishway|dam|system|inlet|harbour|seapen|r|cr|h)\\b") |> 
-      str_replace_all("[:space:]+", " ") |> # Collapse all spaces to single
-      str_trim() # Remove leading/trailing blank spaces
-  ) |> 
   filter(!is.na(resolved_stock_id)) |> 
   stringdist_left_join(
     wcvi_cn_rivers, 
-    by = c(cleaned_stock_id = "wcvi_names"),
+    by = c(resolved_stock_id = "wcvi_names"),
     max_dist = 1,
     ignore_case = TRUE,
     distance_col = "dist"
@@ -471,10 +548,12 @@ trim_data <- full_data |>
     ) |> 
   # Clean up values in age and sex
   mutate(
-    resolved_age = if_else(
-      str_detect(resolved_age, "^(3|4|5|6)"),
-      str_extract(resolved_age, "^.{1}"),
-      NA_character_
+    resolved_age = case_when(
+      # Assume sub1s for partial ages
+      str_detect(resolved_age, "M[:digit:]|[:digit:]M") ~ 
+        as.character(as.numeric(str_extract(resolved_age, "[:digit:]")) + 1),
+      str_detect(resolved_age, "^(3|4|5|6)") ~ str_extract(resolved_age, "^.{1}"),
+      TRUE ~ NA_character_
     ) |> 
       as.numeric()
   ) |> 
@@ -499,7 +578,11 @@ clean_data <- trim_data |>
       str_detect(origin, "(?i)nat") ~ "natural",
       T ~ "unknown"
     ),
-    sample_date = convert_to_date(sample_date), # Convenient janitor fn
+    sample_date = convert_to_datetime( # Convenient janitor fn
+      sample_date,
+      string_conversion_failure = "warning"
+      ) |> 
+      as.Date(), 
     area = str_extract(area, "(2|12)[:graph:]*(?=\\b)"),
     sample_source = case_when(
       str_detect(sample_source, "(?i)(gill|gn)") ~ "gill net",
@@ -507,10 +590,105 @@ clean_data <- trim_data |>
       str_detect(sample_source, "(?i)(troll|tr)") ~ "troll",
       str_detect(sample_source, "(?i)brood|hatch") ~ "hatchery",
       T ~ str_to_lower(sample_source)
-    )
+    ),
+    across(where(is.character), parse_guess)
+  ) |> 
+  # Remove some extreme outlier/error measurements
+  filter(
+    !poh_length > 1300,
+    !(poh_length < 350 & resolved_age > 3)
   )
 
 
 # Plots to examine size-at-age trends -------------------------------------
 
+
+# Question 1: Is there a broad trend evident in all WCVI-origin fish?
+clean_data |> 
+  filter(resolved_age %in% c(3:6)) |> 
+  ggplot(aes(year, poh_length)) +
+  facet_wrap(
+    ~resolved_age, 
+    nrow = 1
+  ) +
+  geom_bin_2d(binwidth = c(1, 50)) +
+  geom_smooth()
+# If anything, this plot show size-at-age increasing
+# However, these data are likely confounded by differences in sample 
+# sources or stock compositions over the years
+
+
+# Question 2: Can we resolve any stock-specific trends in size-at-age?
+clean_data |> 
+  filter(resolved_age %in% c(3:6)) |> 
+  group_by(resolved_stock_id) |> 
+  filter(!length(unique(year)) < 5) %>%
+  split(.$resolved_stock_id) |> 
+  imap(
+    ~ .x |> 
+      ggplot(aes(year, poh_length)) +
+      facet_wrap(
+        ~resolved_age, 
+        nrow = 1
+      ) +
+      geom_point(shape = 21, colour = "black", fill = alpha("white", 0)) +
+      geom_smooth(se = FALSE) +
+      labs(title = .y)
+  )
+# Is the spike in recent years real or driven by a change in sampling methodology?  
+  
+
+# Question 3: Where are the majority of observations coming from over time?
+clean_data |> 
+  count(year, dataset) |> 
+  ggplot(aes(year, dataset)) +
+  geom_tile(aes(fill = n))
+# CREST data takes over after 2018... Could be a different length measure
+
+
+# Question 4: Same as Question 2, but what if we remove CREST data?
+clean_data |> 
+  filter(
+    resolved_age %in% c(3:6),
+    dataset != "CREST/FOS"
+  ) |> 
+  group_by(resolved_stock_id) |> 
+  filter(!length(unique(year)) < 5) %>%
+  split(.$resolved_stock_id) |> 
+  imap(
+    ~ .x |> 
+      ggplot(aes(year, poh_length)) +
+      facet_wrap(
+        ~resolved_age, 
+        nrow = 1
+      ) +
+      geom_point(shape = 21, colour = "black", fill = alpha("white", 0)) +
+      geom_smooth(se = FALSE) +
+      labs(title = .y)
+  )
+
+
+# Question 5: Same as Question 1, but broken out by dataset
+clean_data |> 
+  filter(resolved_age %in% c(3:6)) |> 
+  ggplot(aes(year, poh_length)) +
+  facet_grid(dataset ~ resolved_age) +
+  geom_bin_2d(binwidth = c(1, 50)) +
+  stat_poly_line() +
+  stat_poly_eq()
+# Can anything be done to reduce the noise in the datasets?
+
+
+# Question 6: How are lengths in the CREST data for non-sport samples?
+clean_data |> 
+  filter(
+    resolved_age %in% c(3:6),
+    dataset == "CREST/FOS"
+  ) |> 
+  ggplot(aes(year, poh_length)) +
+  facet_grid(~ sample_source) +
+  geom_bin_2d(binwidth = c(1, 50)) +
+  stat_poly_line() +
+  stat_poly_eq()
+# Can anything be done to reduce the noise in the datasets?
 
