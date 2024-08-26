3239d10090c437685e5219443a97e4992430fb66
Brown <Nicholas.Brown@dfo-mpo.gc.ca>
2024-08-26 15:53:58 -0700

    Finishing touches on Chinook rivers map

--- WCVI Chinook rivers map/02-code_WCVI_Chinook_rivers_map.R
+++ WCVI Chinook rivers map/02-code_WCVI_Chinook_rivers_map.R
@@ -2,7 +2,7 @@
 
 
 pkgs <- c("here", "tidyverse", "readxl", "janitor", "sf", "ggspatial")
-#install.packages
+#install.packages(pkgs)
 
 
 # Load packages
@@ -10,8 +10,8 @@ library(here)
 library(tidyverse); theme_set(theme_bw(base_size = 14))
 library(readxl)
 library(janitor)
-library(sf)
-library(ggspatial)
+library(sf) # Large library providing many tools for working with spatial data 
+library(ggspatial) # Add scalebar and North arrow to plots
 
 
 
@@ -56,16 +56,38 @@ stream_lines <- read_sf(
 
 
 # Define bounding box for Vancouver Island
-vi_coords <- c(xmin = -129, xmax = -122, ymin = 48, ymax = 51)
-vi_bbox <- st_bbox(vi_coords, crs = "WGS84")
+vi_coords <- c(xmin = -129.5, xmax = -123, ymin = 48, ymax = 51)
+vi_bbox <- st_bbox(vi_coords, crs = "NAD83")
 
 
 # Disable spherical geometry
 sf_use_s2(FALSE)
 
 
-# Shapefile for the high-res coastline
+# High-res coastline for Vancouver Island
 vi_coastline <- read_sf(
+  here(
+    "WCVI Chinook rivers map",
+    "FWA_COASTLINES_SP",
+    "FWCSTLNSSP_line.shp"
+  )
+) |> 
+  st_transform(crs = "NAD83") |> 
+  st_crop(vi_bbox) |> 
+  # Constrain data to include only Van Isle watersheds
+  filter(
+    WTRSHDGRPC %in% c(
+      "NEVI", "NIMP", "COMX", "TAHS", "COWN", "VICT", 
+      "SANJ", "PARK", "ALBN", "CLAY", "GOLD", "BRKS",
+      "TSIT", "HOLB", "CAMB", "SALM"
+    )
+  ) |> 
+  st_union() |> 
+  st_polygonize()
+
+
+# Coastline polygons for BC and WA
+full_coastline <- read_sf(
   here(
     "WCVI Chinook rivers map",
     "GSHHS_shp",
@@ -73,7 +95,48 @@ vi_coastline <- read_sf(
     "GSHHS_h_L1.shp"
   )
 ) |> 
-  st_crop(vi_coords)
+  st_transform(crs = "NAD83") |> 
+  st_crop(vi_bbox) 
+
+
+# Find where the two maps cross over and remove the overlapping area
+intersection <- st_intersection(vi_coastline, full_coastline)
+difference <- st_difference(full_coastline, st_union(st_geometry(intersection)))
+
+
+# Shapefile containing DFO hatchery locations
+hatcheries <- read_sf(
+  here(
+    "WCVI Chinook rivers map",
+    "Hatchery_locations_shp",
+    "DFO_Hatchery_locations.shp"
+  )
+) |> 
+  st_set_crs(3857) |> 
+  st_transform(crs = "NAD83") |> 
+  st_crop(vi_bbox) |> 
+  # Filter to include only WCVI facilities
+  filter(
+    Fac_Name %in% c(
+      "Sugsaw Creek Hatchery", "Nitinat River Hatchery", "San Juan River Hatchery",
+      "Sooke River Hatchery", "Robertson Creek Hatchery", "Thornton Creek Hatchery",
+      "Kennedy River Hatchery", "Tofino Hatchery", "Conuma River Hatchery",
+      "Tahsis River Hatchery", "Zeballos River Hatchery", "Marble River Hatchery",
+      "Cordy Creek Hatchery"
+    )
+  ) |> 
+  mutate(
+    `Hatchery program` = factor(
+      Program_De,
+      levels = c(
+        "DFO Major Operations",
+        "Community Economic Development Program",
+        "Aboriginal Fisheries Strategy",
+        "Public Involvement Program"
+      ) 
+    ) |> 
+      fct_rev()
+  )
 
 
 # Enable spherical geometry
@@ -81,25 +144,62 @@ sf_use_s2(TRUE)
 
 
 # Plot
-ggplot(vi_coastline) +
+(cn_rivers_map <- ggplot(full_coastline) +
   geom_sf(
-    data = st_as_sfc(vi_bbox),
-    fill = "lightblue1"
+    colour = NA,
+    fill = "grey70"
   ) +
   geom_sf(
-    colour = "grey50",
-    fill = "grey60"
-    ) +
+    data = vi_coastline, 
+    colour = NA,
+    fill = "grey70"
+  ) +
   geom_sf(
     data = stream_lines,
-    colour = "white"
+    colour = "#08519C"
   ) +
-  annotation_scale(location = "br") +
-  coord_sf(expand = FALSE) +
+  geom_sf(
+    data = hatcheries,
+    aes(colour = `Hatchery program`, size = `Hatchery program`)
+  ) +
+  geom_sf_label(
+    data = filter(hatcheries, FacType2 == "OPS Hatchery"),
+    aes(label = Fac_Name),
+    hjust = 0,
+    vjust = 0,
+    alpha = 0.8
+  ) +
+  annotation_scale(location = "bl") +
+  annotation_north_arrow(
+    location = "tr",
+    style = north_arrow_fancy_orienteering()
+  ) +
   scale_y_continuous(breaks = c(49, 50)) +
-  scale_x_continuous(breaks = c(-124, -126, -128)) +
+  scale_x_continuous(breaks = c(-125, -127)) +
+  scale_size_discrete(range = c(3,6)) +
+  scale_colour_brewer(palette = "Dark2") +
+  coord_sf(expand = FALSE) +
+  labs(x = NULL, y = NULL) +
   theme(
-    panel.background = element_blank(),
-    panel.grid = element_line(colour = "grey70", linewidth = 0.1),
-    panel.ontop = TRUE
+    panel.background = element_rect(fill = "lightblue1"),
+    panel.grid = element_blank(),
+    legend.position = c(0.05, 0.1),
+    legend.justification = c(0, 0),
+    legend.background = element_blank()
   )
+)
+  
+
+# Export the plot
+ggsave(
+  cn_rivers_map,
+  filename = here(
+    "WCVI Chinook rivers map",
+    "R-PLOT_WCVI_Chinook_rivers.png"
+    ),
+  height = 6,
+  width = 8,
+  unit = "in"
+)
+
+
