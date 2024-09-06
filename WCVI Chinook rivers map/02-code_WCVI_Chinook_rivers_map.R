04f7e70cef93397a4af890c1937f0279499f26a8
Brown <Nicholas.Brown@dfo-mpo.gc.ca>
2024-09-06 15:36:44 -0700

    Alternative plot with raster bathymetry layer

--- WCVI Chinook rivers map/02-code_WCVI_Chinook_rivers_map.R
+++ WCVI Chinook rivers map/02-code_WCVI_Chinook_rivers_map.R
@@ -1,7 +1,7 @@
 # Packages ----------------------------------------------------------------
 
 
-pkgs <- c("here", "tidyverse", "readxl", "janitor", "sf", "ggspatial")
+pkgs <- c("here", "tidyverse", "readxl", "janitor", "sf", "stars", "ggspatial")
 #install.packages(pkgs)
 
 
@@ -11,12 +11,13 @@ library(tidyverse); theme_set(theme_bw(base_size = 14))
 library(readxl)
 library(janitor)
 library(sf) # Large library providing many tools for working with spatial data 
+library(stars)
 library(ggspatial) # Add scalebar and North arrow to plots
 
 
 
 
-# Load spatial data for streams and hatchery locations ----------------------------------------
+# Load and plot spatial data ----------------------------------------
 
 
 # List of WCVI Chinook Rivers from Diana's FWA table
@@ -52,9 +53,6 @@ stream_lines <- read_sf(
 
 
 
-# Create basemap and start plotting ---------------------------------------
-
-
 # Define bounding box for Vancouver Island
 vi_coords <- c(xmin = -129, xmax = -122.5, ymin = 48, ymax = 51)
 vi_bbox <- st_bbox(vi_coords, crs = "NAD83")
@@ -68,6 +66,18 @@ vi_coastline <- read_sf(
     "Coastline.shp"
   )
 ) |> 
+  st_transform(crs = "NAD83")
+
+
+# Shapefile with lake polygons
+lakes <- read_sf(
+  here(
+    "WCVI Chinook rivers map",
+    "FWA_LAKES_POLY",
+    "FWLKSPL_polygon.shp"
+  )
+) |> 
+  filter(AREA_SQM > 5e6) |> 
   st_transform(crs = "NAD83")
 
 
@@ -106,12 +116,95 @@ hatcheries <- read_sf(
   )
 
 
-# Plot
-(cn_rivers_map <- ggplot(vi_coastline) +
+# Switch off spherical geometry for cropping
+sf_use_s2(FALSE)
+
+
+# Use lakes to crop land 
+land <- vi_coastline %>%
+  st_difference(., st_union(st_geometry(st_intersection(., lakes)))) 
+
+
+# Bathymetry data
+bathy <- read_stars(
+  here(
+    "WCVI Chinook rivers map",
+    "WCVI_bathy_NOAA.tiff"
+  )
+) |> 
+  st_transform(crs = "NAD83") |> 
+  setNames("depth") |> 
+  mutate(depth = if_else(depth > 0, 0, depth))
+
+
+# Switch spherical geometry back on
+sf_use_s2(TRUE)
+
+
+# Guide that disables background filling in key glyph
+g <- guide_legend(override.aes = list(fill = NA))
+
+
+# Create basemap without bathymetry
+(basemap1 <- ggplot(land) +
+  geom_sf(
+    colour = NA,
+    fill = "grey65"
+  ) +
+  annotation_scale(location = "bl") +
+  annotation_north_arrow(
+    location = "tr",
+    style = north_arrow_fancy_orienteering()
+  ) +
+  coord_sf(
+    expand = FALSE,
+    xlim = c(-129, -122.5), 
+    ylim = c(48, 51)
+  ) +
+  labs(x = NULL, y = NULL) +
+  theme(
+    panel.background = element_rect(fill = "lightblue1"),
+    panel.grid = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    legend.position = c(0.05, 0.1),
+    legend.justification = c(0, 0)
+  )
+)
+
+
+# Create basemap with bathymetry
+(basemap2 <- ggplot(land) +
+    geom_stars(data = bathy) +
     geom_sf(
       colour = NA,
-      fill = "grey70"
+      fill = "grey65"
     ) +
+    annotation_scale(location = "bl") +
+    annotation_north_arrow(
+      location = "tr",
+      style = north_arrow_fancy_orienteering()
+    ) +
+    coord_sf(
+      expand = FALSE,
+      xlim = c(-129, -122.5), 
+      ylim = c(48, 51)
+    ) +
+    labs(x = NULL, y = NULL) +
+    guides(fill = "none") +
+    scale_fill_distiller(palette = "Blues") +
+    theme(
+      panel.grid = element_blank(),
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+      legend.position = c(0.05, 0.1),
+      legend.justification = c(0, 0)
+    )
+)
+
+
+# Plot
+(cn_rivers_map <- basemap1 +
     geom_sf(
       data = stream_lines,
       colour = "#08519C"
@@ -127,41 +220,73 @@ hatcheries <- read_sf(
       vjust = 0,
       alpha = 0.8
     ) +
-    annotation_scale(location = "bl") +
-    annotation_north_arrow(
-      location = "tr",
-      style = north_arrow_fancy_orienteering()
-    ) +
     scale_size_discrete(range = c(3,6)) +
     scale_colour_brewer(palette = "Dark2") +
+    scale_fill_distiller(palette = "Blues") +
+    guides(
+      fill = "none",
+      colour = g,
+      size = g
+    ) +
     coord_sf(
       expand = FALSE,
       xlim = c(-129, -122.5), 
       ylim = c(48, 51)
-      ) +
-    labs(x = NULL, y = NULL) +
-    theme(
-      panel.background = element_rect(fill = "lightblue1"),
-      panel.grid = element_blank(),
-      legend.position = c(0.05, 0.1),
-      legend.justification = c(0, 0),
-      legend.background = element_blank(),
-      axis.text = element_blank(),
-      axis.ticks = element_blank()
+    ) +
+    theme(legend.background = element_blank())
+)
+
+
+# Alternate version with bathymetry added
+(cn_rivers_map_alt <- basemap2 +
+    geom_sf(
+      data = stream_lines,
+      colour = "#08519C"
+    ) +
+    geom_sf(
+      data = hatcheries,
+      aes(colour = `Hatchery program`, size = `Hatchery program`)
+    ) +
+    geom_sf_label(
+      data = filter(hatcheries, FacType2 == "OPS Hatchery"),
+      aes(label = Fac_Name),
+      hjust = 0,
+      vjust = 0,
+      alpha = 0.8
+    ) +
+    scale_size_discrete(range = c(3,6)) +
+    scale_colour_brewer(palette = "Dark2") +
+    scale_fill_distiller(palette = "Blues") +
+    guides(
+      fill = "none",
+      colour = g,
+      size = g
+    ) +
+    coord_sf(
+      expand = FALSE,
+      xlim = c(-129, -122.5), 
+      ylim = c(48, 51)
+    ) +
+    theme(legend.background = element_rect(colour = "black", fill = "white"))
+)
+  
+
+# Export the plots
+list(
+  "WCVI_CN_rivers" = cn_rivers_map,
+  "WCVI_CN_rivers_with_bathy" = cn_rivers_map_alt
+) |> 
+  imap(
+    ~ ggsave(
+      plot = .x,
+      filename = here(
+        "WCVI Chinook rivers map",
+        paste0("R-PLOT_", .y, ".png")
+      ),
+      height = 6,
+      width = 8,
+      unit = "in"
     )
-)
-
-
-# Export the plot
-ggsave(
-  cn_rivers_map,
-  filename = here(
-    "WCVI Chinook rivers map",
-    "R-PLOT_WCVI_Chinook_rivers.png"
-    ),
-  height = 6,
-  width = 8,
-  unit = "in"
-)
+  )
 
 
