36a597601df20413d9b663a2156fbfaabb642b65
Brown <Nicholas.Brown@dfo-mpo.gc.ca>
2024-09-13 12:53:51 -0700

    Use 1% increments in plot; change Srep back to Seq

--- Equilibrium trade-off analysis/02-code_equilibrium_trade-off.R
+++ Equilibrium trade-off analysis/02-code_equilibrium_trade-off.R
@@ -20,14 +20,15 @@ options(scipen = 999)
 # (https://waves-vagues.dfo-mpo.gc.ca/library-bibliotheque/41195255.pdf)
 # Table 10 (Page 56)
 productivity <- tribble(
-  ~cu, ~mean_log_a, ~mean_log_srep, ~log_srep_sd,
+  ~cu, ~mean_log_a, ~mean_log_srep, ~sd_log_srep,
   "SWVI", 1.14, 9.711, 0.286,
   "NWVI", 1.58, 8.196, 0.32,
   "NoKy", 1.53, 10.247, 0.285
 ) |> 
   mutate(
     sd_log_a = 0.5,
-    mean_srep = exp(mean_log_srep - 0.5*log_srep_sd^2)
+    mean_srep = exp(mean_log_srep - 0.5*sd_log_srep^2),
+    sd_srep = exp(sd_log_srep)
   ) |> 
   rowwise() |> 
   # Bootstrap beta values from distribution of alpha and srep
@@ -35,7 +36,7 @@ productivity <- tribble(
     beta = list(
       tibble(
         log_a = rnorm(1000, mean = mean_log_a, sd = sd_log_a),
-        srep = exp(rnorm(1000, mean = mean_log_srep, sd = log_srep_sd))
+        srep = rnorm(1000, mean = mean_srep, sd = sd_srep)
       ) |> 
         mutate(beta = log_a/srep) |> 
         summarize(
@@ -50,21 +51,21 @@ productivity <- tribble(
 # Define functions for calculating Srep and Heq ----------------------------
 
 
-# Srep - equilibrium population escapement at fixed harvest rate
-Srep <- function(alpha, beta, U) {
+# Seq - equilibrium population escapement at fixed harvest rate
+Seq <- function(alpha, beta, U) {
   
-  Srep = (alpha - (-log(1-U)))/beta
+  Seq = (alpha - (-log(1-U)))/beta
   
-  return(Srep) 
+  return(Seq) 
 }
 
 
 # Heq - equilibrium harvest 
 Heq <- function(alpha, beta, U) {
   
-  Srep = (alpha - (-log(1-U)))/beta
+  Seq = (alpha - (-log(1-U)))/beta
   
-  Heq = (Srep * exp(alpha-(beta*Srep))) - Srep
+  Heq = (Seq * exp(alpha-(beta*Seq))) - Seq
   
   return(Heq)
 }
@@ -75,26 +76,26 @@ Heq <- function(alpha, beta, U) {
 
 
 # Define range of harvest rates
-U <- tibble(U = seq(0, 1, by = 0.05))
+U <- tibble(U = seq(0, 0.99, by = 0.01))
 
 
 # Get data summarized by CU
 cu_params <- productivity |> 
-  select(cu, matches("log_(a|srep)")) |> 
+  select(cu, matches("log_a|(?<!log)_srep", perl = TRUE)) |> 
   rowwise() |> 
   mutate(
     data = list(
       tibble(
         log_a = rnorm(1000, mean = mean_log_a, sd = sd_log_a),
-        srep = exp(rnorm(1000, mean = mean_log_srep, sd = log_srep_sd))
+        srep = rnorm(1000, mean = mean_srep, sd = sd_srep)
       ) |> 
         #filter(log_a > 0) |> 
         mutate(beta = log_a/srep) |> 
         crossing(U) |> 
         mutate(
-          Srep = Srep(alpha = log_a, beta = beta, U = U),
+          Seq = Seq(alpha = log_a, beta = beta, U = U),
           Heq = Heq(alpha = log_a, beta = beta, U = U),
-          across(Srep:Heq, ~if_else(U == 1, 0, .x))
+          across(Seq:Heq, ~if_else(U == 1, 0, .x))
         )
     )  
   )
@@ -116,19 +117,19 @@ umsy_vals <- cu_params |>
 # Unpack and summarize data
 eq_sum_data <- cu_params |> 
   unnest(data) |> 
-  filter(!if_any(Srep:Heq, ~(is.na(.x) | is.infinite(.x)))) |> 
+  filter(!if_any(Seq:Heq, ~(is.na(.x) | is.infinite(.x)))) |> 
   mutate(
     .by = cu,
     id = row_number(),
-    across(Srep:Heq, ~if_else(.x < 0, 0, .x))
+    #across(Seq:Heq, ~if_else(.x < 0, 0, .x))
   ) |> 
   summarize(
     .by = c(id, U),
-    across(Srep:Heq, sum)
+    across(Seq:Heq, sum)
   ) |> 
   summarize(
     .by = U,
-    Srep = median(Srep),
+    Seq = median(Seq),
     Heq_mid = median(Heq),
     Heq_lwr = quantile(Heq, 0.25),
     Heq_upr = quantile(Heq, 0.75)
@@ -164,32 +165,37 @@ upr_Heq_Smsy = max(eq_sum_data$Heq_upr)
 
 # label Smsy
 smsy_lab <- tibble(
-  Smsy_mid = eq_sum_data$Srep[eq_sum_data$Heq_mid == mid_Heq_Smsy],
+  Smsy_mid = eq_sum_data$Seq[eq_sum_data$Heq_mid == mid_Heq_Smsy],
   # Heq and Smsy lower and upper bounds are reversed
-  Smsy_upr = eq_sum_data$Srep[eq_sum_data$Heq_lwr == lwr_Heq_Smsy],
-  Smsy_lwr = eq_sum_data$Srep[eq_sum_data$Heq_upr == upr_Heq_Smsy],
+  Smsy_upr = eq_sum_data$Seq[eq_sum_data$Heq_lwr == lwr_Heq_Smsy],
+  Smsy_lwr = eq_sum_data$Seq[eq_sum_data$Heq_upr == upr_Heq_Smsy],
   Heq_mid = mid_Heq_Smsy
 ) |> 
   mutate(label = Smsy_mid)
 
 
+# U labels at 5% increments
+U_lab <- eq_sum_data |> 
+  mutate(U= as.character(U)) |>
+  select(U, Seq) |> 
+  right_join(tibble(U = as.character(seq(0, 0.95, by = 0.05)))) |> 
+  distinct(Seq, .keep_all = TRUE) |> 
+  mutate(U = as.numeric(U))
+
 
 # Plot summarized data
-(eq_plot <- ggplot(eq_sum_data, aes(x = Srep, y = Heq_mid)) +
+(eq_plot <- ggplot(eq_sum_data, aes(x = Seq, y = Heq_mid)) +
     # Add vertical lines showing harvest rate steps
     geom_vline(
-      xintercept = unique(eq_sum_data$Srep),
+      xintercept = U_lab$Seq,
       colour = "grey90"
     ) +
     # Label harvest rate steps
     annotate(
       "text",
-      x = unique(eq_sum_data$Srep),
+      x = U_lab$Seq,
       y = upr_Heq_Smsy,
-      label = eq_sum_data |> 
-        distinct(Srep, .keep_all = TRUE) |> 
-        pull(U) |> 
-        scales::percent(),
+      label = scales::percent(U_lab$U),
       # Make text vertical and offset to the right of the lines
       angle = 270,
       vjust = -0.5,
@@ -205,8 +211,8 @@ smsy_lab <- tibble(
     # Add stepped CI corresponding to the stepped line
     geom_rect(
       aes(
-        xmin = Srep,
-        xmax = lead(Srep),
+        xmin = Seq,
+        xmax = lead(Seq),
         ymin = num_cu_exceed_umsy_lwr*ratio, 
         ymax = num_cu_exceed_umsy_upr*ratio
       ),
@@ -259,6 +265,10 @@ smsy_lab <- tibble(
         name = "Number of CUs where agg. ER > Umsy"
       )
     ) +
+    coord_cartesian(
+      xlim = c(0, max(eq_sum_data$Seq)),
+      ylim = c(0, max(eq_sum_data$Heq_upr*1.05))
+      ) +
     labs(
       y = "Aggregate equilibrium harvest",
       x = "Aggregate equilibrium spawners"
