71e4bb5f0d6fc66034d1cd67d18895acecc7e045
Brown <Nicholas.Brown@dfo-mpo.gc.ca>
2024-09-24 14:01:01 -0700

    Infill escapement time series and delete script for older plot format no longer being used.

--- Kobe plot/02-code_KOBE_plots.R
+++ Kobe plot/02-code_KOBE_plots.R
@@ -7,6 +7,7 @@ pkgs <- c(
 #install.packages(pkgs)
 
 library(tidyverse); theme_set(theme_light(base_size = 18))
+library(magrittr)
 library(ggtext)
 library(janitor)
 library(RColorBrewer)
@@ -16,23 +17,229 @@ library(readxl)
 library(here)
 
 
-# Load and tidy the data --------------------------------------------------
+# Load and infill the escapement data time series ------------------------------------
+
+
+# List of indicator rivers
+indicator_names <- read_xlsx(
+  here("Kobe plot", "01-data_FWA table update version 4 May30.xlsx"),
+  sheet = "RiverEscData",
+  skip = 2
+) |> 
+  clean_names() |> 
+  select(indicator, stream_name) |> 
+  filter(indicator %in% c("Ind7", "Ind10")) |> 
+  pull(stream_name) |> 
+  make_clean_names()
 
 
 # Escapement time series
 indicator_esc <- read_xlsx(
-  here("Kobe plot", "01-data_FWA table update version 4 April17.xlsx"),
+  here("Kobe plot", "01-data_FWA table update version 4 May30.xlsx"),
   sheet = "RiverEscData (2)",
-  skip = 2
+  skip = 2,
+  na = "-"
   ) |> 
   clean_names() |> 
-  select(cu, indicator, watershed, stream_name, brood_year, spawners, natural_origin_spawners) |> 
+  select(-x20) |> 
   rename(
     "river" = stream_name,
     "escapement" = spawners,
-    "year" = brood_year
+    "year" = brood_year,
+    "sep_releases" = sep_releases_ff_smolts_seapen
   ) |> 
   mutate(river = make_clean_names(river, allow_dupes = TRUE))
+
+
+# Filter data to only the 1979+ time series of indicator streams
+trimmed_esc <- indicator_esc |> 
+  filter(
+    year >= 1979, # start the time series in 1979
+    river %in% indicator_names
+  ) |> 
+  arrange(river, year)
+  
+
+# which streams and years need infilling?
+to_infill <- trimmed_esc |> 
+  filter(is.na(escapement)) |> 
+  select(river, year) |> 
+  mutate(infill = "yes")
+
+
+# List return years that received some enhancement during brood years
+enhancement_history <- trimmed_esc |> 
+  filter(sep_releases > 500) |> # Some years received very little enhancement
+  mutate(
+    brood_year = year,
+    return_year1 = year + 3,
+    return_year2 = year + 4,
+    return_year3 = year + 5
+  ) |> 
+  pivot_longer(
+    cols = contains("return_year"),
+    names_prefix = "return_year",
+    values_to = "return_year",
+    names_to = "name"
+  ) |> 
+  select(return_year, river) |> 
+  distinct() |> 
+  mutate(enhanced = "yes")
+
+
+# Are any years available for pAVG calculations?
+trimmed_esc |> 
+  filter(!is.na(escapement)) |> 
+  left_join(
+    enhancement_history,
+    by = join_by(year == return_year, river == river)
+  ) |> 
+  filter(is.na(enhanced)) |> 
+  pivot_wider(
+    id_cols = year,
+    names_from = river,
+    values_from = escapement
+  ) |> 
+  filter(!if_any(!year, is.na))
+# No years where all streams were surveyed and none were receiving enhanced returns
+# Proceed with pMAX method
+
+
+# pMAX data by enhanced versus not
+pmax_data <- trimmed_esc |> 
+  filter(!is.na(escapement)) |> 
+  left_join(
+    enhancement_history,
+    by = join_by(river == river, year == return_year)
+  ) |> 
+  mutate(
+    .by = c(river, enhanced),
+    max = max(escapement, na.rm = TRUE),
+    pmax = escapement/max
+  ) |> 
+  select(river, year, enhanced, max, pmax)
+  
+  
+# List of rivers/years where pmax data are available
+pmax_eligible <- pmax_data |> 
+  mutate(data = if_else(is.na(enhanced), "yes", "yes - enhanced")) |> 
+  select(year, river, data)
+
+
+# Need to assign neighbouring streams for pMAX infilling
+pmax_neighbours <- to_infill |> 
+  distinct(river) |> 
+  # Manually assign neighbouring streams for pMAX 
+  mutate(
+    neighbours = case_when(
+      river == "artlish_river" ~ list(c("kaouk_river", "tahsish_river")),
+      river == "bedwell_ursus" ~ list(c("tranquil_creek", "moyeha_river", "megin_river")),           
+      river == "burman_river" ~ list(c("gold_river_aggregate")),            
+      river == "colonial_cayeghle_creeks" ~ list(c("marble_river", "artlish_river", "tahsish_river")),
+      river == "gold_river_aggregate" ~ list(c("burman_river")),    
+      river == "kaouk_river" ~ list(c("artlish_river", "tahsish_river")),             
+      river == "leiner_river" ~ list(c("tahsis_river", "zeballos_river")),            
+      river == "megin_river" ~ list(c("moyeha_river", "bedwell_ursus", "tranquil_creek")),             
+      river == "moyeha_river" ~ list(c("megin_river", "bedwell_ursus", "tranquil_creek")),            
+      river == "nahmint_river" ~ list(c("sarita_river", "tranquil_creek")),           
+      river == "sarita_river" ~ list(c("nahmint_river", "tranquil_creek")),            
+      river == "tahsis_river" ~ list(c("leiner_river", "zeballos_river")),            
+      river == "tranquil_creek" ~ list(c("bedwell_ursus", "nahmint_river", "sarita_river")),          
+      river == "zeballos_river" ~ list(c("leiner_river", "tahsis_river"))
+    )
+  ) |> 
+  separate(
+    neighbours,
+    into = c("neighbour1", "neighbour2", "neighbour3"),
+    sep = ","
+  ) |> 
+  mutate(
+    across(
+      contains("neighbour"),
+      ~str_remove_all(.x, "c\\(|\"|\\)") |> # Remove leftover symbols from character vectors
+        str_trim()
+    )
+  )
+
+
+# Put pieces together and select years/rivers for pMAX infilling
+pmax_infill_logic <- to_infill |> 
+  left_join(pmax_neighbours) |> 
+  left_join(
+    enhancement_history,
+    by = join_by(river == river, year == return_year)
+  ) |> 
+  left_join(
+    rename(pmax_eligible, "data1" = data),
+    by = join_by(neighbour1 == river, year == year)
+  ) |> 
+  left_join(
+    rename(pmax_eligible, "data2" = data),
+    by = join_by(neighbour2 == river, year == year)
+  ) |> 
+  left_join(
+    rename(pmax_eligible, "data3" = data),
+    by = join_by(neighbour3 == river, year == year)
+  ) |> 
+  arrange(river, year) %T>%
+  # Some rivers/years still have no good neighbour:
+  {print(filter(., if_all(contains("data"), is.na)))} |> # Megin, Moyeha, and Bedwell in 1984
+  mutate(
+    neighbour4 = if_else(
+      str_detect(river, "bedwell|megin|moyeha") & year == 1984,
+      "sarita_river",
+      NA_character_
+    )
+  ) |> 
+  left_join(
+    rename(pmax_eligible, "data4" = data),
+    by = join_by(neighbour4 == river, year == year)
+  ) |> 
+  pivot_longer(
+    matches("neighbour|data"),
+    names_pattern = "(.+)(\\d)",
+    names_to = c(".value", "set"),
+    values_drop_na = TRUE
+  ) |> 
+  filter(!is.na(data)) |> 
+  # Stipulate rules for which data to use for pMAX infilling
+  mutate(
+    .by = c(river, year),
+    options = n(),
+    choice = case_when(
+      options == 1 ~ "keep",
+      options > 1 & is.na(enhanced) & data == "yes" ~ "keep",
+      options > 1 & enhanced == "yes" & data == "yes - enhanced" ~ "keep",
+      options > 1 & is.na(enhanced) & all(data == "yes - enhanced") ~ "keep",
+      TRUE ~ "drop"
+    )
+  )
+
+
+# Infill escapement data using neighbouring pMAX values
+infilled_esc <- pmax_infill_logic |> 
+  # Drop extra options for infill data by prioritizing lowest sets
+  # i.e. first neighbour listed with good data is preferred
+  arrange(river, year, set) |> 
+  distinct(river, year, .keep_all = TRUE) |> 
+  select(river, year, infill, enhanced, neighbour) |> 
+  # Add the pMAX value for selected neighbouring populations
+  left_join(
+    select(pmax_data, river, year, pmax),
+    by = join_by(neighbour == river, year == year)
+  ) |> 
+  # Add each system's own max value
+  left_join(distinct(pmax_data, river, enhanced, max)) |> 
+  mutate(escapement = max * pmax) |> 
+  select(river, year, infill, escapement) |> 
+  bind_rows(
+    trimmed_esc |> 
+      filter(!is.na(escapement)) |> 
+      select(river, year, escapement)
+  )
+  
+  
+# Load ER data time series and build dataframe for Kobe plot ---------------
 
 
 # Estimated Robertson CN exploitation rate time series
@@ -44,53 +251,50 @@ rch_er <- read.csv(here("Kobe plot", "01-data_2024ERA_MREERdata_11.06.2024.csv")
 
 # Reference points data set
 indicator_rp <- read_xlsx(
-  here("Kobe plot", "01-data_FWA table update version 4 April17.xlsx"),
+  here("Kobe plot", "01-data_FWA table update version 4 May30.xlsx"),
   sheet = "RiverEscData",
   skip = 2
 ) |> 
   clean_names() |> 
   select(cu, indicator, watershed, stream_name, matches("life_cycle|lc")) |> 
-  separate_wider_regex(
-    cols = contains("range"),
-    patterns = c("\\(", lwr = ".*", "-", upr = ".*", "\\)"),
-    names_sep = "_"
-  ) |> 
-  rename_with(
-    ~str_remove_all(.x, "(?<=s.{3}_).*(?=upr|lwr)") |> 
-      str_remove_all("_life_cycle")
-  ) |> 
   rename("river" = stream_name) |> 
-  # Add SR parameters
+  # Clean up river names
   mutate(
-    smsy_divide_srep = smsy/srep,
-    alpha = exp((0.5-smsy_divide_srep)/0.07),
+    river = make_clean_names(river),
+    across(contains("lc"), as.numeric)
+  ) |> 
+  filter(river %in% indicator_names) |> 
+  summarize(
+    smsy = sum(lc_smsy_median),
+    srep = sum(lc_srep_median)
+  ) |> 
+  # Calculate SR parameters
+  mutate(
+    alpha = exp((0.5-smsy/srep)/0.07),
     beta = log(alpha)/srep,
     umsy = (0.5*log(alpha)) - (0.07*(log(alpha))^2),
-    recruits_at_0.85smsy = alpha*0.85*smsy*(exp(-beta*0.85*smsy))
-  ) |> 
-  # Clean up river names
-  mutate(river = make_clean_names(river))
-
+  )
+  
 
 # Make dataframe for KOBE plot
-kobe_data <- indicator_esc |> 
-  filter(
-    indicator %in% c("Ind7", "Ind17"),
-    !is.na(escapement)
-  ) |> 
-  left_join(select(.data = indicator_rp, river, umsy, smsy)) |> 
-  mutate(river = str_remove_all(river, "(?<=_).*") |> str_remove_all("_")) |> 
+kobe_data <- infilled_esc |> 
   summarize(
     .by = year,
     escapement = sum(escapement),
-    umsy = mean(umsy),
-    smsy = sum(smsy),
-    rivers = paste0(river, collapse = "; ")
+    umsy_lc = indicator_rp$umsy,
+    smsy_lc = indicator_rp$smsy,
+    umsy_rr = 0.57,
+    smsy_rr = 18999
   ) |> 
   left_join(rch_er) |> 
+  pivot_longer(
+    matches("(u|s)msy"),
+    names_sep = "_",
+    names_to = c(".value", "method")
+  ) |> 
   mutate(
-    y = er/umsy,
     x = escapement/smsy,
+    y = er/umsy,
     quadrant = case_when(
       y >= 1 & x <= 1 ~ 1,
       y >= 1 & x > 1 ~ 5, # Nothing falls in this zone
@@ -104,48 +308,15 @@ kobe_data <- indicator_esc |>
       quadrant == 3 ~ "darkorange2",
       quadrant == 4 ~ "springgreen3",
       quadrant == 5 ~ "darkgoldenrod1" # Nothing falls in this quadrant
-    )
+    ),
+    method = factor(
+      method, 
+      levels = c("lc", "rr"), 
+      labels = c("Life cycle (low productivity)", "Run reconstruction (moderate productivity)")
+      )
   ) |> 
-  relocate(rivers, .after = last_col()) |> # Move "rivers" list to the end
   # Remove years with missing data
   filter(!if_any(!year, is.na)) |> 
-  arrange(year)
-
-
-# Alternate Kobe data using the equilibrium harvest rate approach
-kobe_data_alt <- indicator_esc |> 
-  filter(
-    indicator %in% c("Ind7", "Ind17"),
-    !is.na(escapement)
-  ) |> 
-  mutate(river = str_remove_all(river, "(?<=_).*") |> str_remove_all("_")) |>
-  summarize(
-    .by = year,
-    escapement = sum(escapement)
-  ) |> 
-  left_join(rch_er) |>
-  mutate(
-    smsy = 20167,
-    umsy = 0.55,
-    y = er/umsy,
-    x = escapement/smsy,
-    quadrant = case_when(
-      y >= 1 & x <= 1 ~ 1,
-      y >= 1 & x > 1 ~ 5, # Nothing falls in this zone
-      y < 1 & between(x,0.8,1) ~ 2, # Amber zone
-      y < 1 & x <= 0.8 ~ 3,
-      y < 1 & x > 1 ~ 4
-    ) %>% factor(),
-    colour = case_when(
-      quadrant == 1 ~ "firebrick2",
-      quadrant == 2 ~ "darkgoldenrod1",
-      quadrant == 3 ~ "darkorange2",
-      quadrant == 4 ~ "springgreen3",
-      quadrant == 5 ~ "darkgoldenrod1" # Nothing falls in this quadrant
-    )
-  ) |> 
-  # Remove years with missing data
-  filter(!if_any(!year, is.na))  |> 
   arrange(year)
 
 
@@ -159,16 +330,6 @@ writexl::write_xlsx(
 )
 
 
-# Save alternate output data
-writexl::write_xlsx(
-  select(.data = kobe_data_alt, -colour, -quadrant),
-  here(
-    "Kobe plot", 
-    paste0("R-OUT_Kobe_plot_raw_data_alternate_", Sys.Date(), ".xlsx")
-  )
-)
-
-
 # Plots -------------------------------------------------------------------
 
 
@@ -176,8 +337,8 @@ writexl::write_xlsx(
 quadLabs <- data.frame(
   label = c(
     "Overfishing",
-    "Overfishing but spawning\nbiomass sufficient",
-    "Fisheries reduced to\nallow rebuilding",
+    "Overfishing but spawning<br>biomass sufficient",
+    "Fisheries reduced to<br>allow rebuilding",
     "Sustainable fishery"
   ),
   x = c(0.1, 1.9, 0.1, 1.9),
@@ -186,153 +347,46 @@ quadLabs <- data.frame(
 ) 
 
 
-# Base KOBE plot
-(base_kobe <- kobe_data %>% 
-    arrange(year) %>% 
-    ggplot(aes(x, y)) +
-    geom_hline(
-      yintercept = 1,
-      lty = 2,
-      colour = "grey70"
-    ) +
-    geom_vline(
-      xintercept = 1, 
-      lty = 2, 
-      colour = "grey70"
-    ) +
-    annotate(
-      "rect", 
-      xmin = 0.8, 
-      xmax = 1, 
-      ymin = -1, 
-      ymax = 1, 
-      alpha = .2
-    ) +
-    annotate(
-      "text",
-      label = "85*\'%\'~S[MSY]", 
-      x = 0.9, 
-      y = 0.25, 
-      angle = 90,
-      parse = TRUE
-    ) +
-    geom_text(
-      data = quadLabs, 
-      aes(
-        label = label,
-        hjust = hjust
-      )
-    ) +
-    coord_fixed(
-      ratio = 1,
-      xlim = c(0,2),
-      ylim = c(0,2),
-      expand = FALSE
-    ) +
-    scale_size_continuous(labels = scales::comma) +
-    guides(
-      colour = "none",
-      alpha = "none"
-    ) +
-    labs(
-      x = "Spawner abundance / Spawner abundance at MSY",
-      y = "Fishery ER / ER at MSY",
-      size = "Spawner\nabundance"
-    ) +
-    theme(
-      legend.position = c(0.65,0.7),
-      legend.background = element_rect(colour="black"),
-      panel.grid = element_blank()
-    )
-)
-
-
-# Add points and path
-(filled_kobe <- base_kobe +
-    geom_path(aes(alpha = year)) +
-    geom_point(
-      shape = 21,
-      fill = kobe_data$colour,
-      alpha = 0.75,
-      size = 3,
-      colour = ifelse(
-        kobe_data$year %in% c(min(kobe_data$year), max(kobe_data$year)),
-        "blue",
-        alpha(kobe_data$colour, 0.75)
-      )
-    ) +
-    geom_label_repel(
-      data = filter(kobe_data, year %in% c(min(year), max(year))),
-      aes(label = year),
-      colour = "blue",
-      fill = alpha("white", alpha = 0.75),
-      size = 5,
-      min.segment.length = 0,
-      segment.colour = "blue",
-      segment.size = 0.75,
-      segment.curvature = 0.03,
-      segment.angle = 90,
-      box.padding = 0.7,
-      label.size = 0
-    ) +
-    scale_alpha_continuous(range = c(0.025,0.8))
-)
-
-
-# Save the filled Kobe plot
-ggsave(
-  here("Kobe plot", "R-PLOT_WCVI_CN_Nat_Kobe.png"),
-  filled_kobe,
-  height = 7,
-  width = 7,
-  units = "in"
-)
-
-
-# Animated version
-# (animated_kobe <- base_kobe +
-#     geom_path(aes(alpha = year)) +
-#     geom_point(
-#       colour = kobe_data$colour,
-#       size = 3,
-#     ) +
-#     scale_alpha_continuous(range = c(0.025,0.8)) +
-#     transition_reveal(along = year, keep_last = T) +
-#     labs(subtitle = 'Year: {frame_along}') 
-# )
-# 
-# # Animate it
-# anim <- animate(animated_kobe, duration = 60, fps = 20, 
-#                 width = 13.3333, height = 7.5, units = "in", 
-#                 res = 150, # Resolution is in pixels-per-inch (ppi)
-#                 renderer = gifski_renderer(), 
-#                 end_pause = 20*10) # End pause is in frames: 20 frames * 10 = 10 seconds
-# 
-# anim_save(here("Kobe plot", "KOBE_ts_animation.gif"), animation = anim)
-
-
-
-# Code from FSAR group ----------------------------------------------------
-
-
-kobe_dfs <- list("RR" = kobe_data_alt, "LifeCycle" = kobe_data) |> 
-  list_rbind(names_to = "method") |> 
-  mutate(
-    method = if_else(
-    method == "RR", 
-    "Run reconstruction (moderate productivity)", 
-    "Life cycle (low productivity)"
-    )
-  )
-
-
-
-(kobe_plots <- kobe_dfs |> 
+# Inspired by code from FSAR group
+(kobe_plots <- kobe_data |> 
     ggplot(aes(x, y)) +
     facet_wrap(~method, nrow = 1) +
     #add "crosshairs"
-    geom_vline(xintercept = 1, lty = 2) +
-    geom_hline(yintercept = 1, lty = 2) +
+    geom_vline(
+      xintercept = 1, 
+      lty = 2,
+      colour = "grey50"
+    ) +
+    geom_hline(
+      yintercept = 1, 
+      lty = 2,
+      colour = "grey50"
+    ) +
+    # Label Umsy on plot
+    geom_text(
+      data = distinct(kobe_data, method, umsy),
+      aes(
+        label = paste0("U[MSY]==", 100*round(umsy, 2), "*\'%\'"),
+        x = 1.5,
+        y = 1
+      ),
+      colour = "grey50",
+      vjust = -0.5,
+      parse = TRUE
+    ) +
+    # Label Smsy on plot
+    geom_text(
+      data = distinct(kobe_data, method, smsy),
+      aes(
+        label = paste0("S[MSY]==", smsy),
+        x = 1,
+        y = 1.5
+      ),
+      colour = "grey50",
+      vjust = -0.5,
+      angle = 90,
+      parse = TRUE
+    ) +
     geom_path(aes(alpha = year)) + #if you want to connect the dots
     geom_point(aes(color = year), size=3) +
     annotate(
@@ -352,14 +406,14 @@ kobe_dfs <- list("RR" = kobe_data_alt, "LifeCycle" = kobe_data) |>
       parse = TRUE
     ) +
     geom_richtext(
-      data = filter(kobe_dfs, year== min(year)|year== max(year)),
+      data = filter(kobe_data, year== min(year)|year== max(year)),
       aes(
         label = rep(
           c(
-            paste0("'", str_sub(min(kobe_dfs$year), 3L)), 
-            paste0("'", str_sub(max(kobe_dfs$year), 3L)) 
+            paste0("'", str_sub(min(kobe_data$year), 3L)), 
+            paste0("'", str_sub(max(kobe_data$year), 3L)) 
           ), 
-          2
+          each = 2
         )
       ),
       hjust = 0-.2, 
@@ -368,18 +422,20 @@ kobe_dfs <- list("RR" = kobe_data_alt, "LifeCycle" = kobe_data) |>
       fill = alpha("white", 0.75)
     ) +
     geom_point(
-      data = filter(kobe_dfs, year== min(year)|year== max(year)),
+      data = filter(kobe_data, year== min(year)|year== max(year)),
       colour = "red",
       shape = 21,
       size = 3,
       stroke = 1.25
     ) +
-    geom_text(
+    geom_richtext(
       data = quadLabs, 
       aes(
         label = label,
         hjust = hjust
-      )
+      ),
+      label.colour = NA,
+      fill = alpha("white", 0.75)
     ) +
     coord_fixed(
       ratio = 1,
@@ -398,14 +454,14 @@ kobe_dfs <- list("RR" = kobe_data_alt, "LifeCycle" = kobe_data) |>
     scale_alpha(range = c(0.05, 0.85)) +
     guides(alpha = "none") +
     labs(
-      y = expression(U/U[MSY]), 
-      x = expression(S/S[MSY]),
+      y = expression(frac(Exploitation~rate, U[MSY])), 
+      x = expression(frac(Spawner~abundance,S[MSY])),
       parse = TRUE
-      ) +
+    ) +
     theme_bw(base_size = 12) +
     theme(
       legend.position = "top",
-      panel.spacing.x = unit(1.5, "lines")
+      panel.grid = element_blank()
     )
 )
 
@@ -418,7 +474,7 @@ kobe_plots |>
       paste0("R-PLOT_WCVI_CN_two-methods.png")
     ),
     height = 7,
-    width = 14,
+    width = 12,
     units = "in",
     dpi = "print"
   )
