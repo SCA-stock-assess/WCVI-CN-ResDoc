2f9af6fd383914896aeb1a8b5e0a2d64cc464909
Brown <Nicholas.Brown@dfo-mpo.gc.ca>
2024-10-03 11:53:35 -0700

    Add data source information and update plot to include legend

--- Survival time series/RCH_CWT_survival_plotting.R
+++ Survival time series/RCH_CWT_survival_plotting.R
@@ -6,7 +6,7 @@ pkgs <- c("here", "tidyverse")
 
 # Load packages
 library(here)
-library(tidyverse); theme_set(theme_bw(base_size = 14))
+library(tidyverse); theme_set(theme_bw(base_size = 12))
 
 
 
@@ -20,10 +20,17 @@ surv_data <- here(
 ) |> 
   read.csv() |> 
   rename(
-    "Survival to age-2" = age2_surv,
-    "Survival to adult return" = adult_surv
+    "Survival to age-2_CWT" = age2_surv,
+    "Survival to adult return_CWT" = adult_surv,
+    "Survival to adult return_OTO" = adult_surv_nat_stamp
   ) |> 
-  pivot_longer(cols = !brood_year)
+  pivot_longer(cols = !brood_year) |> 
+  mutate(
+    method = if_else(str_detect(name, "_CWT"), "CWT", "Otolith mark"),
+    name = str_remove_all(name, "_.*")
+  ) |> 
+  # Keep complete brood years only
+  filter(brood_year <= 2023 - 5)
 
 
 # Time series plot
@@ -37,24 +44,29 @@ surv_data <- here(
     scales = "free_y",
     ncol = 1
   ) +
-  geom_line(linewidth = 1) +
-  geom_point(
-    data = filter(
-      surv_data,
-      name == "Survival to adult return",
-      brood_year > 2019
-    )
+  geom_line(
+    aes(lty = method),
+    linewidth = 1
   ) +
-  scale_y_continuous(labels = scales::percent) +
+  scale_y_continuous(
+    labels = scales::percent,
+    expand = expansion(mult = c(0, 0.05))
+  ) +
   labs(
     y = NULL,
-    x = "Ocean-entry year"
+    x = "Ocean-entry year",
+    lty = "Data source"
   ) +
   theme(
     strip.placement = "outside",
-    strip.background = element_blank()
+    strip.background = element_blank(),
+    legend.position = "inside",
+    legend.position.inside = c(0.02, 0.98),
+    legend.justification = c(0, 1),
+    legend.background = element_rect(colour = "black")
   )
 )
+
 
 # Export the plot
 ggsave(
