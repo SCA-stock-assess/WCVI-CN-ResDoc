42bd4e5a6537164d8d3875d3a8b9cda2c7727eb9
Nick Brown <nickawbrown@gmail.com>
2024-11-25 13:50:15 -0800

    Add trendline for 7 core indicators to 4-panel plot

--- 2. R code/FSAR 4-panel plot/WCVI_CN_4-panel_plot_code.R
+++ 2. R code/FSAR 4-panel plot/WCVI_CN_4-panel_plot_code.R
@@ -1,10 +1,11 @@
 # Packages ----------------------------------------------------------------
 
-pkgs <- c("readxl", "tidyverse", "ggpubr", "here")
+pkgs <- c("readxl", "tidyverse", "ggpubr", "here", "janitor")
 #install.packages(pkgs)
 
 library(here)
 library(tidyverse); theme_set(theme_bw(base_size = 8))
+library(janitor)
 library(readxl)
 library(ggpubr)
 library(ggtext)
@@ -14,11 +15,39 @@ library(ggtext)
 # Import data -------------------------------------------------------------
 
 
+# Escapement data from infilling method
+esc_time_series <- here(
+  "3. R outputs", 
+  "Escapement infill", 
+  "R-OUT_infilled_indicators_escapement_timeseries.csv"
+) |> 
+  read.csv() |> 
+  mutate(
+    core = if_else(
+      str_detect(river, "(?i)bedwell|megin|moyeha|artlsih|kaouk|tahsish|marble"),
+      1,
+      0
+    ) 
+  ) 
+
+
+# Summarize data across all 17 indicators, and across only the core 7
+esc_summary <- esc_time_series |> 
+  summarize(
+    .by = year,
+    nat_escapement = sum(escapement),
+    nat_escapement_core = sum(escapement[core == 1])
+  )
+
+
+# Read the data file with ocean harvest and ER data, then merge
+# escapement data from the infill process
 data <- here(
   "1. data files", 
   "FSAR_4-panel_data.xlsx"
 ) |> 
   read_xlsx() |> 
+  left_join(esc_summary) |> 
   filter(!if_any(everything(), is.na)) |> 
   pivot_longer(!year) %>%
   split(.$name) |> 
@@ -34,13 +63,51 @@ data <- here(
   ) |> 
   mutate(
     y = if_else(name == "ocean_catch", y/1000, y),
+    group = if_else(name == "nat_escapement_core", "core", "all"),
     name = case_when(
       name == "ocean_er" ~ "Exploitation rate in ocean fisheries",
       name == "ocean_catch" ~ "Catch in ocean fisheries (1000s)",
-      name == "nat_escapement" ~ "Aggregate spawner abundance index",
+      str_detect(name, "nat_escapement") ~ "Aggregate spawner abundance index",
       name == "recruitment" ~ "Recruitment"
     )
   )
+
+
+# Biological benchmark data
+bb <- here("1. data files", "WCVI_CN_streams_master_list.xlsx") |> 
+  read_xlsx() |> 
+  clean_names() |> 
+  filter(
+    str_detect(
+      mainstem,
+      "(?i)artlish|bedwell|burman|cayeghle|gold|kaouk|leiner|marble|megin|
+      moyeha|nahmint|sarita|san.*juan|tahsis|tahsish|tranquil|zeballos"
+    )
+  )
+
+
+# Biological benchmarks for indicator populations
+Umsy <- 0.43
+Umsy_lci <- 0.30
+Umsy_uci <- 0.54
+
+Smsy_17 <- bb |> 
+  pull(smsy_lc) |> 
+  sum()
+
+Sgen_17 <- bb |> 
+  pull(sgen_lc) |> 
+  sum()
+
+Smsy_7 <- bb |> 
+  filter(str_detect(mainstem, "(?i)bedwell|megin|moyeha|artlsih|kaouk|tahsish|marble")) |> 
+  pull(smsy_lc) |> 
+  sum()
+
+Sgen_7 <- bb |> 
+  filter(str_detect(mainstem, "(?i)bedwell|megin|moyeha|artlsih|kaouk|tahsish|marble")) |> 
+  pull(sgen_lc) |> 
+  sum()
 
 
 
@@ -53,14 +120,23 @@ plots <- data |>
   rowwise() |> 
   mutate(
     plot = list(
-      ggplot(data, aes(x = x, y = y)) +
+      ggplot(
+        data, 
+        aes(
+          x = x, 
+          y = y, 
+          colour = group
+        )
+      ) +
         geom_line(linewidth = 0.75) +
         scale_y_continuous(expand = expansion(mult = (c(0, 0.07)))) +
+        scale_colour_manual(values = c("black", "grey70")) +
         coord_cartesian(ylim = c(0, max(data$y))) +
         labs(
           x = NULL,
           y = name
         ) +
+        guides(colour = "none") +
         theme(
           axis.title = element_text(size = 9),
           panel.grid = element_blank()
@@ -74,22 +150,6 @@ plots <- data |>
 # Make specific adjustments to each panel
 (plots[[3]] <- plots[[3]] +
     scale_y_continuous(labels = scales::percent) +
-    geom_hline(
-      yintercept = c(0.44),
-      colour = c("blue"),
-      lty = 2,
-      linewidth = 0.5
-    ) +
-    # Add inter-quartile confidence band around Umsy 
-    annotate(
-      "rect",
-      xmin = -Inf,
-      xmax = Inf,
-      ymin = 0.30,
-      ymax = 0.54,
-      fill = "blue",
-      alpha = 0.15
-    ) +
     annotate(
       "richtext",
       x = max(data$x, na.rm = TRUE),
@@ -103,36 +163,66 @@ plots <- data |>
         #"*U*<sub>MSY</sub> based on moderate productivity",
         "*U*<sub>MSY</sub> based on low productivity"
       ),
-      colour = c("blue"),
+      colour = c("black"),
       label.colour = NA,
-      fill = NA,
+      fill = alpha("white", 0.8),
       size = 2.5
-    )
+    ) +
+    geom_hline(
+      yintercept = c(0.44),
+      colour = c("black"),
+      lty = 2,
+      linewidth = 0.5
+    ) +
+    # Add inter-quartile confidence band around Umsy 
+    annotate(
+      "rect",
+      xmin = -Inf,
+      xmax = Inf,
+      ymin = 0.30,
+      ymax = 0.54,
+      fill = "black",
+      alpha = 0.10
+    ) 
 )
 
 
 (plots[[1]] <- plots[[1]] +
-    geom_hline(
-      yintercept = c(19107*0.85, 8493),
-      colour = c("blue", "red"),
-      lty = 2,
-      linewidth = 0.5
-    ) +
     annotate(
       "richtext",
       x = min(data$x, na.rm = TRUE),
-      y = c(19107*0.85, 8493)+250,
+      y = c(
+        Smsy_17*0.85, Sgen_17
+        #,Smsy_7*0.85, Sgen_7
+        )+250,
       vjust = 0,
       hjust = 0,
       label = c(
         "85% *S*<sub>MSY</sub> based on low productivity",
         "*S*<sub>gen</sub> based on low productivity"
+        #,"85% *S*<sub>MSY</sub> based on low productivity",
+        #"*S*<sub>gen</sub> based on low productivity"
       ),
-      colour = c("blue", "red"),
+      colour = c(
+        "black", "black"
+        #,"grey50", "grey50"
+      ),
       label.colour = NA,
       fill = alpha("white", alpha = 0.8),
       size = 2.5
-    )
+    ) +
+    geom_hline(
+      yintercept = c(
+        Smsy_17*0.85, Sgen_17
+        #,Smsy_7*0.85, Sgen_7
+      ),
+      colour = c(
+        "black", "black"
+        #,"grey70", "grey70"
+      ),
+      lty = 2,
+      linewidth = 0.5
+    ) 
 )
 
 
